4e8db1fddf25d5d130e16f19fa63e6dc67faca85
Brian Thomas <atomos86@live.com>
2014-12-09 15:54:36 -0500

    Updates gitaliases so gstandup will run correctly

--- aliases/gitaliases.zsh
+++ aliases/gitaliases.zsh
@@ -8,7 +8,7 @@ alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
-alias gstandup="!git log –-since yesterday –-author `git config user.email` –-pretty=short"
+alias gstandup="git log --since=1.day.ago --author `git config user.email` --pretty=short"
 alias gupp='git fetch -p && gup'
 
 
